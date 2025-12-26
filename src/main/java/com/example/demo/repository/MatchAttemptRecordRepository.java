304eea4c08cba99dbc1802e8ec26cc95231ecdc6
bala-amypo <bala@amypo.in>
2025-12-26 11:34:55 +0000

    User commit

--- src/main/java/com/example/demo/repository/MatchAttemptRecordRepository.java
+++ src/main/java/com/example/demo/repository/MatchAttemptRecordRepository.java
@@ -3,10 +3,6 @@ package com.example.demo.repository;
 import com.example.demo.model.MatchAttemptRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface MatchAttemptRecordRepository
         extends JpaRepository<MatchAttemptRecord, Long> {
-
-    List<MatchAttemptRecord> findByInitiatorStudentIdOrCandidateStudentId(Long a, Long b);
 }
