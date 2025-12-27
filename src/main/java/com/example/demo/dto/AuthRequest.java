881d9f24c37f68806358877a06ad7fb8972a1c03
bala-amypo <bala@amypo.in>
2025-12-27 04:01:59 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -7,15 +7,38 @@ public class AuthRequest {
     private String email;
     private String role;
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public AuthRequest() {
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getUsername() {
+        return username;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
