fd3f0b3baad85663b8cd6fecf146172f598e1c78
bala-amypo <bala@amypo.in>
2025-12-27 05:22:41 +0000

    User commit

--- src/main/java/com/example/demo/model/CompatibilityScoreRecord.java
+++ src/main/java/com/example/demo/model/CompatibilityScoreRecord.java
@@ -10,33 +10,28 @@ public class CompatibilityScoreRecord {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "student_a_id", nullable = false)
+    @Column(name = "student_a_id")
     private Long studentAId;
 
-    @Column(name = "student_b_id", nullable = false)
+    @Column(name = "student_b_id")
     private Long studentBId;
 
-    @Column(name = "score", nullable = false)
-    private Integer score;
+    @Column(name = "score")
+    private Double score;
 
     public CompatibilityScoreRecord() {}
 
-    public CompatibilityScoreRecord(Long id, Long studentAId, Long studentBId, Integer score) {
-        this.id = id;
+    public CompatibilityScoreRecord(Long studentAId, Long studentBId, Double score) {
         this.studentAId = studentAId;
         this.studentBId = studentBId;
         this.score = score;
     }
 
     public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
     public Long getStudentAId() { return studentAId; }
     public void setStudentAId(Long studentAId) { this.studentAId = studentAId; }
-
     public Long getStudentBId() { return studentBId; }
     public void setStudentBId(Long studentBId) { this.studentBId = studentBId; }
-
-    public Integer getScore() { return score; }
-    public void setScore(Integer score) { this.score = score; }
+    public Double getScore() { return score; }
+    public void setScore(Double score) { this.score = score; }
 }
--- src/main/java/com/example/demo/model/RoomAssignmentRecord.java
+++ src/main/java/com/example/demo/model/RoomAssignmentRecord.java
@@ -10,33 +10,28 @@ public class RoomAssignmentRecord {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "student_a_id", nullable = false)
+    @Column(name = "student_a_id")
     private Long studentAId;
 
-    @Column(name = "student_b_id", nullable = true)
+    @Column(name = "student_b_id")
     private Long studentBId;
 
-    @Column(name = "room_number", nullable = false)
+    @Column(name = "room_number")
     private String roomNumber;
 
     public RoomAssignmentRecord() {}
 
-    public RoomAssignmentRecord(Long id, Long studentAId, Long studentBId, String roomNumber) {
-        this.id = id;
+    public RoomAssignmentRecord(Long studentAId, Long studentBId, String roomNumber) {
         this.studentAId = studentAId;
         this.studentBId = studentBId;
         this.roomNumber = roomNumber;
     }
 
     public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
     public Long getStudentAId() { return studentAId; }
     public void setStudentAId(Long studentAId) { this.studentAId = studentAId; }
-
     public Long getStudentBId() { return studentBId; }
     public void setStudentBId(Long studentBId) { this.studentBId = studentBId; }
-
     public String getRoomNumber() { return roomNumber; }
     public void setRoomNumber(String roomNumber) { this.roomNumber = roomNumber; }
 }
