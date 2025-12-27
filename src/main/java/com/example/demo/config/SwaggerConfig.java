ebda221676356ba5f0b5ab5888af6e027535f5fd
bala-amypo <bala@amypo.in>
2025-12-27 06:59:14 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -16,25 +16,32 @@ public class SwaggerConfig {
     private static final String SECURITY_SCHEME_NAME = "BearerAuth";
 
     @Bean
-    public OpenAPI api() {
+    public OpenAPI openAPI() {
         return new OpenAPI()
                 .servers(List.of(
                         new Server()
-                                .url("https://9096.32procr.amypo.ai")
+                                .url("http://localhost:8080")
                                 .description("Local Development Server")
                 ))
-                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
-                                new SecurityScheme()
-                                        .name("Authorization")
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                        ))
-                .info(new Info()
-                        .title("Hostel Roommate Compatibility Matcher API")
-                        .description("API for matching hostel roommates based on compatibility")
-                        .version("1.0"));
+                .addSecurityItem(
+                        new SecurityRequirement().addList(SECURITY_SCHEME_NAME)
+                )
+                .components(
+                        new io.swagger.v3.oas.models.Components()
+                                .addSecuritySchemes(
+                                        SECURITY_SCHEME_NAME,
+                                        new SecurityScheme()
+                                                .name("Authorization")
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .info(
+                        new Info()
+                                .title("Hostel Roommate Compatibility Matcher API")
+                                .description("API for matching hostel roommates based on compatibility")
+                                .version("1.0")
+                );
     }
 }
